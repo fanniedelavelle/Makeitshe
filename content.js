899cc32d978495463ee168a3d2d679e908be3fb0
Nadia <ncampowoytuk@gmail.com>
2017-09-26 01:50:58 +0200

    Added style.css with a sample red color format for replaced words. Now when a “he” is replaced it is also assigned the class “replacement” which can be formatted in css.

--- content.js
+++ content.js
@@ -1,22 +1,23 @@
-//get all elements from the html 
+//get all elements from the html
 var elements = document.getElementsByTagName('*');
-// loop through the html tags 
+// loop through the html tags
 for (var i = 0; i < elements.length; i++) {
-    var element = elements[i];
-    // loop inside the tags for child nodes 
-    for (var j = 0; j < element.childNodes.length; j++) {
-        var node = element.childNodes[j];
-        // if the element is text get its value and replace the text with something else. 
-        if (node.nodeType === 3) {
-            var text = node.nodeValue;
-            var he = /(\she\s)/gi;
-            for (var h = 0; h < text.length; h++){
-                var replacedText = text.replace(he,  " SHE ");
-                //var new_html = "<span><strong>" + replacedText + "</strong></span>";
-            }
-            if (replacedText !== text) {
-                element.replaceChild(document.createTextNode(replacedText), node);
-            }
+  var element = elements[i];
+  // loop inside the tags for child nodes
+  for (var j = 0; j < element.childNodes.length; j++) {
+    var node = element.childNodes[j];
+    // if the element is text get its value and replace the text with something else.
+    if (node.nodeType === 3) {
+      var text = node.nodeValue;
+      // Search for the words to be replaced
+      var s = text.search(/\bhe\b/gi);
+      // If the text doesn't contain the words, skip it
+      if(s!=-1){
+        if(element!=null){
+          // Replace HTML in order to create a new "span" for each replacement
+          element.innerHTML = element.innerHTML.replace(/\bhe\b/gi, '<span class="replacement"> she </span>');
         }
+      }
     }
+  }
 }
