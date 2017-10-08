fef26cf5ad821e5d267b539caa40b900642fe52f
Nadia <ncampowoytuk@gmail.com>
2017-10-08 21:57:38 +0200

    Added data/words.js as a male to female words dictionary. Easy to add new words.

--- content.js
+++ content.js
@@ -1,37 +1,75 @@
-// Male words
-var m_words = ["he", "He", "his", "His", "father", "Father", "Brother", "brother", "Uncle", "Aunt", "Male", "male", "Mr", "Mister", "Sir", "actor", "Actor", "boy", "Boy", "congressman", "Congressman"];
+// Male words stats variables
 var m_percent;
 var m_count = 0;
-// Female words
-var f_words = ["she", "She", "her", "Her", "mother", "Mother", "Sister", "brother", "Aunt", "aunt", "Female", "female", "Ms", "Madam", "Madam", "Actress", "Actress", "girl", "Girl", "Congresswoman", "Congresswoman"];
+// Female words stats variables
 var f_percent;
 var f_count = 0;
+
+// Json with words dictionary
+var words = window.word_dict;
 
 // Get all elements from the html
 var elements = document.getElementsByTagName('*');
 
-// FEMALE WORD LOOP: count the female words before replacing the male words
-// loop through the html tags
-for (var i = 0; i < elements.length; i++) {
-  var element = elements[i];
-  // loop inside the tags for child nodes
-  for (var j = 0; j < element.childNodes.length; j++) {
-    var node = element.childNodes[j];
-    // if the element is text get its value and replace the text with something else.
-    if (node.nodeType === 3) {
-      var text = node.nodeValue;
-      // Search for the female words
-      for (var k = 0; k < f_words.length; k++) {
-        // Search for the m¡f_word
-        var m = f_words[k];
-        var s = new RegExp("\\b" + m + "\\b");
-        // Get the position where the word is
-        var s_pos = text.search(s);
-        // If the text doesn't contain the words, skip it
-        if (s_pos != -1) {
-          if (element != null) {
-            // Count it, don't replace!
-            f_count++;
+var countFemale = function() {
+
+  // FEMALE WORD LOOP: count the female words before replacing the male words
+  // loop through the html tags
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    // loop inside the tags for child nodes
+    for (var j = 0; j < element.childNodes.length; j++) {
+      var node = element.childNodes[j];
+      // if the element is text get its value and replace the text with something else.
+      if (node.nodeType === 3) {
+        var text = node.nodeValue;
+        // Search for the female words
+        for (m_word in words) {
+          var f_word = words[m_word];
+          var s = new RegExp("\\b" + f_word + "\\b");
+          // Get the position where the word is
+          var s_pos = text.search(s);
+          // If the text doesn't contain the words, skip it
+          if (s_pos != -1) {
+            if (element != null) {
+              // Count it, don't replace!
+              f_count++;
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+var swapMale = function() {
+  // MALE WORD LOOP
+  // loop through the html tags
+  for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+    // loop inside the tags for child nodes
+    for (var j = 0; j < element.childNodes.length; j++) {
+      var node = element.childNodes[j];
+
+      // if the element is text get its value and replace the text with something else.
+      if (node.nodeType === 3) {
+        var text = node.nodeValue;
+
+        // Search for the male words to be replaced
+        for (m_word in words) {
+          var f_word = words[m_word];
+          var s = new RegExp("\\b" + m_word + "\\b");
+          // Get the position where the word is
+          var s_pos = text.search(s);
+          // If the text doesn't contain the words, skip it
+          if (s_pos != -1) {
+            if (element != null) {
+              // Replace HTML in order to create a new "span" for each replacement
+              // Replace the respective f_word
+              s_replace = '<span class="replacement tooltip">' + f_word + '</span>';
+              element.innerHTML = element.innerHTML.replace(s, s_replace);
+              m_count++;
+            }
           }
         }
       }
@@ -40,39 +78,10 @@ for (var i = 0; i < elements.length; i++) {
 }
 
 
-// MALE WORD LOOP
-// loop through the html tags
-for (var i = 0; i < elements.length; i++) {
-  var element = elements[i];
-  // loop inside the tags for child nodes
-  for (var j = 0; j < element.childNodes.length; j++) {
-    var node = element.childNodes[j];
+// Count female words and swap male words
+countFemale();
+swapMale();
 
-    // if the element is text get its value and replace the text with something else.
-    if (node.nodeType === 3) {
-      var text = node.nodeValue;
-
-      // Search for the male words to be replaced
-      for (var k = 0; k < m_words.length; k++) {
-        // Search for the m_word
-        var m = m_words[k];
-        var s = new RegExp("\\b" + m + "\\b");
-        // Get the position where the word is
-        var s_pos = text.search(s);
-        // If the text doesn't contain the words, skip it
-        if (s_pos != -1) {
-          if (element != null) {
-            // Replace HTML in order to create a new "span" for each replacement
-            // Replace the respective f_word
-            s_replace = '<span class="replacement tooltip">' + f_words[k] + '</span>';
-            element.innerHTML = element.innerHTML.replace(s, s_replace);
-            m_count++;
-          }
-        }
-      }
-    }
-  }
-}
 console.log("Done replacing");
 
 // Calculate percentages
