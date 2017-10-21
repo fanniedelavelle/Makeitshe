286d072750281699a266c33f8e81da2a3a33836d
Nadia <ncampowoytuk@gmail.com>
2017-10-21 17:24:19 +0200

    Combined name swapping and word swapping!!!

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
+// For timing performance
+var t0 = performance.now();
+
 
 // Male words stats variables
 var m_percent;
@@ -6,15 +9,32 @@ var m_count = 0;
 var f_percent;
 var f_count = 0;
 
-// Json with words dictionary
+// Json with names
+var names = window.name_dict;
+// MALE
+var ignore_names = ['San Diego', 'San Francisco', 'New York', 'Hillary Clinton'];
+var ignore_regex = new RegExp(ignore_names.join("|"), "g");
+var names_regex = new RegExp(Object.keys(names).join(" |") + '(\.|,|;|:)?', "g");
+// FEMALE
+var f_ignore_names = [];
+var f_ignore_regex = new RegExp(ignore_names.join("|"), "g");
+var f_names_regex = new RegExp(Object.values(names).join(" |") + '(\.|,|;|:)?', "g");
+
+
+// Json with words
 var words = window.word_dict;
+// MALE
+var words_regex = new RegExp("\\b" + Object.keys(words).join("\\b|\\b"), "gi");
+// FEMALE
+var f_words_regex = new RegExp("\\b" + Object.values(words).join("\\b|\\b"), "gi");
+
+
 
 // Get all elements from the html
 var elements = document.getElementsByTagName('*');
 
-var countFemale = function() {
-
-  // FEMALE WORD LOOP: count the female words before replacing the male words
+// Count all female words
+var countFemale = function(regex) {
   // loop through the html tags
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
@@ -24,78 +44,75 @@ var countFemale = function() {
       // if the element is text get its value and replace the text with something else.
       if (node.nodeType === 3) {
         var text = node.nodeValue;
-        // Search for the female words
-        for (m_word in words) {
-          var f_word = words[m_word];
-          var s = new RegExp("\\b" + f_word + "\\b");
-          // Get the position where the word is
-          var s_pos = text.search(s);
-          // If the text doesn't contain the words, skip it
-          if (s_pos != -1) {
-            if (element != null) {
-              // Count it, don't replace!
-              f_count++;
-            }
-          }
+        if (!ignore_regex.test(text)) {
+          new_string = text.replace(regex, function(matched) {
+            // Just count! don't replace
+            f_count++;
+          });
         }
       }
-    }
+    };
   }
 }
 
-var swapMale = function() {
-  // MALE WORD LOOP
+// Replace function
+function replaceAll(str, mapObj, regex) {
+  var mod = false;
+  if (!ignore_regex.test(str)) {
+    new_string = str.replace(regex, function(matched) {
+      m_count++;
+      var replacement = mapObj[matched.trim()];
+      // If it can't find it it's in uppercase:
+      if (replacement == null) {
+        // match to lower case word
+        replacement = mapObj[matched.toLowerCase().trim()];
+        // replace with upper case word
+        replacement = replacement.charAt(0).toUpperCase() + replacement.slice(1);
+      }
+      mod = true;
+      return '<span class="replacement"> ' + replacement + ' </span>';
+    });
+  }
+  if (mod) {
+    return new_string;
+  } else {
+    return false;
+  }
+}
+
+// Find function
+var findAll = function(mapObj, regex) {
   // loop through the html tags
   for (var i = 0; i < elements.length; i++) {
     var element = elements[i];
     // loop inside the tags for child nodes
     for (var j = 0; j < element.childNodes.length; j++) {
       var node = element.childNodes[j];
-
       // if the element is text get its value and replace the text with something else.
       if (node.nodeType === 3) {
         var text = node.nodeValue;
-        // Search for the male words to be replaced
-        for (m_word in words) {
-          var f_word = words[m_word];
-          var s = new RegExp("\\b" + m_word + "\\b");
-          // Get the position where the word is
-          var s_pos = text.search(s);
-          // If the text doesn't contain the words, skip it
-          if (s_pos != -1) {
-            if (element != null) {
-              // Replace HTML in order to create a new "span" for each replacement
-              // Replace the respective f_word
-              if (element.nodeName == "TITLE") {
-                // If male word is in title, don't format it
-                s_replace = f_word;
-                document.title = document.title.replace(s, s_replace);
-              } else if (element.nodeName == "TEXTAREA") {
-                // If in a text area, such as a tweet, don't replace!
-              } else {
-                // Any other type of text, OK to replace and format
-                s_replace = '<span class="replacement tooltip">' + f_word + '</span>';
-                element.innerHTML = element.innerHTML.replace(s, s_replace);
-              }
-              m_count++;
-            }
-          }
+        var updated_text = replaceAll(text, mapObj, regex);
+        if (element != null && updated_text != false) {
+          element.innerHTML = updated_text;
         }
       }
     }
   }
-}
+};
 
 
-// Count female words and swap male words
-countFemale();
-swapMale();
-
-console.log("Done replacing");
+countFemale(f_names_regex);
+countFemale(f_words_regex);
+findAll(names, names_regex);
+findAll(words, words_regex);
 
 // Calculate percentages
 m_percent = Math.round(m_count / (m_count + f_count) * 100);
 f_percent = Math.round(f_count / (m_count + f_count) * 100);
+
+var t1 = performance.now();
+console.log("Finding and replacing all values took " + (t1 - t0) + " milliseconds.");
+console.log("Found "+m_count +" male values and "+f_count+" female values.");
 
 
 // MESSAGING
