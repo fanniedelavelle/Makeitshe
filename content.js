3e476016438c9f5ced15755dbb0d50fc67e30cbb
chloejl <jugband.blues@hotmail.com>
2017-10-28 13:42:50 -0400

    sketchy master merge

--- content.js
+++ content.js
@@ -1,44 +1,163 @@
-// Male words
-var m_words = ["he", "He", "his", "His", "father", "Father", "Brother", "brother", "Uncle", "Aunt","Male", "male","Mr", "Mister", "Sir", "actor", "Actor"];
-// Female words
-var f_words = ["she", "She", "her", "Her", "mother","Mother","Sister", "brother","Aunt", "aunt", "Female", "female","Ms", "Madam", "Madam", "Actress", "Actress"];
+var btnsrc = '<div id="make_it_she_wrap"><input type="button" id="make_it_she" value="Make it She"/></div>';
 
-//get all elements from the html
-var elements = document.getElementsByTagName('*');
+document.body.insertAdjacentHTML('beforeEnd', btnsrc);
 
-// loop through the html tags
-for (var i = 0; i < elements.length; i++) {
-  var element = elements[i];
-  // loop inside the tags for child nodes
-  for (var j = 0; j < element.childNodes.length; j++) {
-    var node = element.childNodes[j];
+document.getElementById('make_it_she').addEventListener("click", function(){  
 
-    // if the element is text get its value and replace the text with something else.
-    if (node.nodeType === 3) {
-      var text = node.nodeValue;
 
-      // Search for the words to be replaced
-      for(var k = 0; k < m_words.length; k++){
+    // For timing performance
+    var t0 = performance.now();
 
-        // Search for the m_word
-        var m = m_words[k];
-        var s = new RegExp("\\b"+m+"\\b");
-        // Get the position where the word is
-        var s_pos = text.search(s);
 
-        // If the text doesn't contain the words, skip it
-        if(s_pos!=-1){
-          if(element!=null){
-            // Replace HTML in order to create a new "span" for each replacement
-            // Replace the respective f_word
-            s_replace = '<span class="replacement">' + f_words[k] + '</span>';
-            element.innerHTML = element.innerHTML.replace(s, s_replace);
+    // Male words stats variables
+    var m_percent;
+    var m_count = 0;
+    // Female words stats variables
+    var f_percent;
+    var f_count = 0;
+
+    // Json with names
+    var names = window.name_dict;
+    // MALE
+    var ignore_names = ['San Diego', 'San Francisco', 'New York', 'Hillary Clinton'];
+    var ignore_regex = new RegExp(ignore_names.join("|"), "g");
+    var names_regex = new RegExp(Object.keys(names).join(" |") + '(\.|,|;|:)?', "g");
+    // FEMALE
+    var f_ignore_names = [];
+    var f_ignore_regex = new RegExp(ignore_names.join("|"), "g");
+    var f_names_regex = new RegExp(Object.values(names).join(" |") + '(\.|,|;|:)?', "g");
+
+
+    // Json with words
+    var words = window.word_dict;
+    // MALE
+    var words_regex = new RegExp("\\b" + Object.keys(words).join("\\b|\\b"), "gi");
+    // FEMALE
+    var f_words_regex = new RegExp("\\b" + Object.values(words).join("\\b|\\b"), "gi");
+
+
+
+    // Get all elements from the html
+    var elements = document.getElementsByTagName('*');
+
+    // Count all female words
+    var countFemale = function(regex) {
+      // loop through the html tags
+      for (var i = 0; i < elements.length; i++) {
+        var element = elements[i];
+        // loop inside the tags for child nodes
+        for (var j = 0; j < element.childNodes.length; j++) {
+          var node = element.childNodes[j];
+          // if the element is text get its value and replace the text with something else.
+          if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            if (!ignore_regex.test(text)) {
+              new_string = text.replace(regex, function(matched) {
+                // Just count! don't replace
+                f_count++;
+              });
+            }
+          }
+        };
+      }
+    }
+
+    // Replace function
+    function replaceAll(str, mapObj, regex) {
+      var mod = false;
+      if (!ignore_regex.test(str)) {
+        new_string = str.replace(regex, function(matched) {
+          m_count++;
+          var replacement = mapObj[matched.trim()];
+          // If it can't find it it's in uppercase:
+          if (replacement == null || replacement == undefined) {
+            // match to lower case word
+            replacement = mapObj[matched.toLowerCase().trim()];
+            // replace with upper case word
+            replacement = replacement.charAt(0).toUpperCase() + replacement.slice(1);
+          }
+
+          if (element.nodeName == "TITLE") {
+            // If male word is in title, don't format it
+            document.title = document.title.replace(matched, replacement + " ");
+            mod = true;
+            return replacement + " ";
+          } else if (element.nodeName == "TEXTAREA") {
+            // If in a text area, such as a tweet, don't replace!
+            mod = true;
+            return matched;
+          } else {
+            // Any other type of text, OK to replace and format
+            mod = true;
+             return '<span class="replacement">' + replacement + '<span class="tooltiptext">'+ replacement + '</span>' + '</span>';
+          }
+        });
+      }
+      if (mod) {
+        return new_string;
+      } else {
+        return false;
+      }
+    }
+
+    var element;
+    // Find function
+    var findAll = function(mapObj, regex) {
+      // loop through the html tags
+      for (var i = 0; i < elements.length; i++) {
+        element = elements[i];
+        // loop inside the tags for child nodes
+        for (var j = 0; j < element.childNodes.length; j++) {
+          var node = element.childNodes[j];
+           if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var updated_text = replaceAll(text, mapObj, regex);
+            if (element != null && updated_text != false) {
+              element.innerHTML = element.innerHTML.replace(text, updated_text);
+              // element.innerHTML = updated_text;
+            }
           }
         }
       }
-    }
-  }
-}
+    };
 
 
-console.log("Done replacing");
+    countFemale(f_names_regex);
+    countFemale(f_words_regex);
+    findAll(names, names_regex);
+    findAll(words, words_regex);
+
+    // Calculate percentages
+    m_percent = Math.round(m_count / (m_count + f_count) * 100);
+    f_percent = Math.round(f_count / (m_count + f_count) * 100);
+
+    var t1 = performance.now();
+    console.log("Finding and replacing all values took " + (t1 - t0) + " milliseconds.");
+    console.log("Found " + m_count + " male values and " + f_count + " female values.");
+
+
+    // MESSAGING
+    // In order to communicate between the content script and the popup
+
+    // Inform the background page that this tab should have a page-action
+    chrome.runtime.sendMessage({
+      from: 'content',
+      subject: 'updateStats'
+    });
+
+    // Listen for messages from the popup
+    chrome.runtime.onMessage.addListener(function(msg, sender, response) {
+      // First, validate the message's structure
+      if ((msg.from === 'popup') && (msg.subject === 'stats')) {
+        // Collect the necessary data
+        var stats = {
+          male: m_percent,
+          female: f_percent,
+        };
+
+        // Directly respond to the sender (popup),
+        // through the specified callback */
+        response(stats);
+      }
+    });
+});
---
+++ popup.js
@@ -0,0 +1,67 @@
+var malep = document.getElementById("malep");
+var femalep = document.getElementById("femalep");
+
+// Stats hidden at start
+document.getElementById("stats").style.display = "none";
+
+
+// Update the relevant fields with the new data
+function setStats(stats) {
+  malep.innerHTML = stats.male;
+  femalep.innerHTML = stats.female;
+  // Set tweet text
+  setTweet(stats.male, stats.female);
+}
+
+// Once the DOM is ready...
+window.addEventListener('DOMContentLoaded', function() {
+  // ...query for the active tab...
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(tabs) {
+    // ...and send a request for the DOM info...
+    chrome.tabs.sendMessage(
+      tabs[0].id, {
+        from: 'popup',
+        subject: 'stats'
+      },
+      // ...also specifying a callback to be called
+      //    from the receiving end (content script)
+      setStats);
+  });
+});
+
+
+// TWEET
+
+var tweetB = document.getElementById("tweet-button");
+var tweetText;
+var tweetUrl;
+
+tweetB.onclick = function() {
+
+  chrome.tabs.create({
+    active: true,
+    url: tweetUrl
+  });
+};
+
+function setTweet(m, f) {
+  tweetText = m + "% mentions of men vs " + f + "% women on this page. Let's bridge the gender gap!";
+  var activeTab;
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, function(arrayOfTabs) {
+    // since only one tab should be active and in the current window at once
+    // the return variable should only have one entry
+    activeTab = arrayOfTabs[0];
+
+    // Set the tweet text and parameters
+    console.log(activeTab.url);
+    tweetUrl = "https://twitter.com/intent/tweet?text=" + encodeURIComponent(tweetText) + "&url=" + activeTab.url + "&via=makeitshe";
+    document.getElementById("stats").style.display = "block";
+    tweetB.style.color = "red";
+  });
+}
